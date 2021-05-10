2275aacd386338a4721eb218682fa5c0eff7b5f4
Nik Charlebois <nicharl@microsoft.com>
2021-05-10 09:51:26 -0400

    Update GetServicePrincipalId.ps1

--- Utilities/Scripts/GetServicePrincipalId.ps1
+++ Utilities/Scripts/GetServicePrincipalId.ps1
@@ -4,11 +4,14 @@ param(
     [string] [Parameter(Mandatory=$true)] $AppToGet
 )
 
-Install-Module Microsoft.Graph.Authentication -Force
-Import-Module Microsoft.Graph.Authentication -Force
-Install-Module Microsoft.Graph.Applications -Force
-Import-Module Microsoft.Graph.Applications -Force
+Write-Output "Installing Modules..."
+Install-Module Microsoft.Graph.Authentication -Force | Out-Null
+Import-Module Microsoft.Graph.Authentication -Force | Out-Null
+Install-Module Microsoft.Graph.Applications -Force | Out-Null
+Import-Module Microsoft.Graph.Applications -Force | Out-Null
+Write-Output "Done"
 
+Write-Output "Authenticating to the Microsoft Graph..."
 $url = "https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token"
 $body = @{
     scope = "https://graph.microsoft.com/.default"
@@ -21,7 +24,10 @@ $body = @{
 $OAuthReq = Invoke-RestMethod -Uri $url -Method Post -Body $body
 $AccessToken = $OAuthReq.access_token
 Connect-MgGraph -AccessToken $AccessToken | Out-Null
+Write-Output "Done"
 
+Write-Output "Obtaining Service Principal Info..."
 $application = Get-MgServicePrincipal -All:$true -Filter "AppID eq '$AppToGet'"
 $DeploymentScriptOutputs = @{}
 $DeploymentScriptOutputs['PrincipalId'] = $application.Id
+Write-Output "Done {$application.Id}"
